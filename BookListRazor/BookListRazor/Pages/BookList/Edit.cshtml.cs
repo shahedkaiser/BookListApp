ed9294acf26a1713d6aa4a6b903f6e2e9a4c39fe
unknown <shahedkaiser@hotmail.com>
2019-09-13 20:30:04 +0600

    First Commit

---
+++ BookListRazor/BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [TempData]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book =await _db.Book.FindAsync(id);
+            //Book = _db.Book.Where(b => b.Id == id).FirstOrDefault();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDb =await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.ISBN = Book.ISBN;
+                BookFromDb.Author = Book.Author;
+                await _db.SaveChangesAsync();
+                Message = "Book has been edited successfully.";
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
---
+++ BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if(book==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _db.Book.Remove(book);
+                await _db.SaveChangesAsync();
+                Message = "Book has been deleted successfully.";
+                return RedirectToPage("Index");
+            }
+        }
+    }
+}
